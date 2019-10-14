7c9277dede2c50e99e0ccc229f33f19402b79caa
Zacharey Carey <zac4ncstate@aol.com>
2019-10-14 18:44:31 -0400

    components

--- congoer/src/App.js
+++ congoer/src/App.js
@@ -33,3 +33,47 @@ class App extends Component {
 }
 
 export default App;
+
+// export default App;
+
+// import React, { Component } from 'react';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// injectTapEventPlugin();
+// import './App.css';
+// import Loginscreen from './Loginscreen'
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+//     this.state={
+//       loginPage:[],
+//       uploadScreen:[]
+//     }
+//   }
+//   componentWillMount(){
+//     var loginPage =[];
+//     loginPage.push(<Loginscreen parentContext={this}/>);
+//     this.setState({
+//                   loginPage:loginPage
+//                     })
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         {this.state.loginPage}
+//         {this.state.uploadScreen}
+//       </div>
+//     );
+//   }
+// }
+// const style = {
+//   margin: 15,
+// };
+// export default App;
+
+// if(response.data.code == 200){
+//   console.log("Login successfull");
+//   var uploadScreen=[];
+//   uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
+//   self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
+//   }
